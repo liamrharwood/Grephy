83e01ccd9c26997b65da4266c0eef44b0ab955c9
liamrharwood <liamrharwood@gmail.com>
2018-05-05 18:24:38 -0400

    Implement DFA minimization

--- src/main/java/grephy/DFA.java
+++ src/main/java/grephy/DFA.java
@@ -1,5 +1,6 @@
 package grephy;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class DFA extends NFA {
@@ -52,5 +53,121 @@ public class DFA extends NFA {
                 }
             }
         }
+
+        minimize(alphabet);
+    }
+
+    private void minimize(List<Character> alphabet) {
+        ArrayList<ArrayList<Integer>> partitions = new ArrayList();
+        partitions.add(new ArrayList(acceptingStates));
+        partitions.add(new ArrayList());
+        for (int i = 0; i < states.size(); i++) {
+            if (!acceptingStates.contains(i)) {
+                partitions.get(1).add(i);
+            }
+        }
+
+        ArrayList<ArrayList<Integer>> waiting = new ArrayList();
+        waiting.add(new ArrayList(acceptingStates));
+        while (!waiting.isEmpty()) {
+            ArrayList<Integer> A = waiting.remove(0);
+            for (Character c : alphabet) {
+                //let X be the set of states for which a transition on c leads to a state in A
+                HashSet<Integer> X = new HashSet();
+                for (Integer state : states) {
+                    ArrayList<Transition> ts = new ArrayList(delta);
+                    ts.removeIf(t -> t.symbol.get() != c || !A.contains(t.stateTo));
+                    for (Transition t : ts) {
+                        X.add(t.stateFrom);
+                    }
+                }
+
+                int i = 0;
+                while (i < partitions.size()) {
+                    ArrayList<Integer> Y = new ArrayList(partitions.get(i));
+                    ArrayList<Integer> X2 = new ArrayList(X);
+                    X2.retainAll(Y);
+                    ArrayList<Integer> Y2 = new ArrayList(Y);
+                    Y2.removeAll(X);
+                    if (!X2.isEmpty() && !Y2.isEmpty()) {
+                        partitions.remove(i);
+                        partitions.add(X2);
+                        partitions.add(Y2);
+
+                        boolean flag = false;
+                        int j = 0;
+                        while (j < waiting.size()) {
+                            HashSet<Integer> set = new HashSet(waiting.get(j));
+                            if (set.containsAll(Y) && Y.containsAll(set)) {
+                                waiting.remove(j);
+                                waiting.add(X2);
+                                waiting.add(Y2);
+                                flag = true;
+                                break;
+                            }
+                            j++;
+                        }
+                        if (!flag) {
+                            if (X2.size() <= Y2.size()) {
+                                waiting.add(X2);
+                            } else {
+                                waiting.add(Y2);
+                            }
+                        }
+                    }
+
+                    i++;
+                }
+            }
+        }
+        mergeStates(partitions, alphabet);
+    }
+
+    private void mergeStates(List<ArrayList<Integer>> partitions, List<Character> alphabet) {
+        ArrayList<Integer> newStates = new ArrayList();
+        ArrayList<Transition> newDelta = new ArrayList();
+        ArrayList<Integer> newAcceptingStates = new ArrayList();
+
+        int i = 0;
+        while (i < partitions.size()) {
+            if (partitions.get(i).contains(0)) {
+                partitions.add(0, partitions.get(i));
+                partitions.remove(i+1);
+                break;
+            }
+            i++;
+        }
+
+        for (i = 0; i < partitions.size(); i++) {
+            for (int j = 0; j < partitions.get(i).size(); j++) {
+                if (acceptingStates.contains(partitions.get(i).get(j))) {
+                    newAcceptingStates.add(i);
+                    break;
+                }
+            }
+            newStates.add(i);
+        }
+
+        for (i = 0; i < newStates.size(); i++) {
+            Integer s = partitions.get(i).get(0);
+            for (Character c : alphabet) {
+                ArrayList<Transition> transitions = new ArrayList(delta);
+                transitions.removeIf(t -> t.stateFrom != s || t.symbol.get() != c);
+                Transition t = transitions.get(0);
+                int j = 0;
+                while (j < partitions.size()) {
+                    if (partitions.get(j).contains(t.stateTo)) {
+                        newDelta.add(new Transition(i, j, t.symbol));
+                        break;
+                    }
+                    j++;
+                }
+            }
+
+        }
+
+        states = newStates;
+        delta = newDelta;
+        acceptingStates = newAcceptingStates;
     }
 }
