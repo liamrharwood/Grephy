f90faf29909ffef1534e16029f9662fbc61bc35e
liamrharwood <liamrharwood@gmail.com>
2018-05-06 15:22:15 -0400

    Add comments to Transition class

--- src/main/java/grephy/Transition.java
+++ src/main/java/grephy/Transition.java
@@ -2,10 +2,13 @@ package grephy;
 
 import java.util.Optional;
 
+/**
+ * Transition.java - Represents state transitions in an NFA / DFA.
+ */
 public class Transition {
     public int stateFrom;
     public int stateTo;
-    public Optional<Character> symbol;
+    public Optional<Character> symbol; // Can be empty in the case of epsilon transitions
 
     public Transition(int stateFrom, int stateTo, Optional<Character> symbol) {
         this.stateFrom = stateFrom;
